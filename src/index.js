b00e9112fbc2f6f44dda72be5afdc7310fae26a6
Chris Benjamin <cbenjamin@stormseed.com>
2018-01-05 11:38:27 -0500

    update imports on index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import Calendar from './calendar/Calendar'
+import Calendar from './components/calendar/Calendar'
+import CalendarAgenda from './components/calendar/CalendarAgenda'
+import CalendarAgendaEvent from './components/calendar/CalendarAgendaEvent'
+import CalendarAllDayEvents from './components/calendar/CalendarAllDayEvents'
+import CalendarCell from './components/calendar/CalendarCell'
+import CalendarDayColumn from './components/calendar/CalendarDayColumn'
+import CalendarDayLabels from './components/calendar/CalendarDayLabels'
+import CalendarEvent from './components/calendar/CalendarEvent'
+import CalendarHeaderNav from './components/calendar/CalendarHeaderNav'
+import CalendarMonth from './components/calendar/CalendarMonth'
+import CalendarTimeLabelColumn from './components/calendar/CalendarTimeLabelColumn'
+import CalendarWeek from './components/calendar/CalendarWeek'
 
 export {
   Calendar,
