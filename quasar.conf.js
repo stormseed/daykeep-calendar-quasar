4d1546a8eb9eba08933acaf13bc14339fba1e939
sirbeagle <cbenjamin@stormseed.com>
2019-02-24 18:19:28 -0500

    move in demo page from quasar-calendar-demo with appropriate modifications

--- quasar.conf.js
+++ quasar.conf.js
@@ -2,19 +2,13 @@
 
 module.exports = function (ctx) {
   return {
-    plugins: [
-      'i18n',
-      'axios'
-    ],
+    plugins: [],
     css: [
-      // 'app.styl'
+      'app.styl'
     ],
     extras: [
       ctx.theme.mat ? 'roboto-font' : null,
       'material-icons'
-      // 'ionicons',
-      // 'mdi',
-      // 'fontawesome'
     ],
     supportIE: false,
     vendor: {
@@ -28,6 +22,8 @@ module.exports = function (ctx) {
       // analyze: true,
       // extractCSS: false,
       // useNotifier: false,
+      publicPath: '/quasar-calendar',
+      distDir: 'docs',
       extendWebpack (cfg) {
         cfg.module.rules.push({
           enforce: 'pre',
@@ -42,37 +38,7 @@ module.exports = function (ctx) {
     //   port: 8084,
     //   open: true // opens browser window automatically
     // },
-    framework: {
-      iconSet: 'material-icons'
-    },
-    // framework: 'all', // --- includes everything; for dev only!
-    // framework: {
-    //   components: [
-    //     'QLayout',
-    //     'QLayoutHeader',
-    //     'QLayoutDrawer',
-    //     'QPageContainer',
-    //     'QPage',
-    //     'QToolbar',
-    //     'QToolbarTitle',
-    //     'QBtn',
-    //     'QIcon',
-    //     'QList',
-    //     'QListHeader',
-    //     'QItem',
-    //     'QItemMain',
-    //     'QItemSide'
-    //   ],
-    //   directives: [
-    //     'Ripple'
-    //   ],
-    //   plugins: [
-    //     'Notify'
-    //   ]
-    // },
-    // animations: 'all' --- includes all animations
-    animations: [
-    ],
+    framework: 'all', // --- includes everything; for dev only!
     pwa: {
       cacheExt: 'js,html,css,ttf,eot,otf,woff,woff2,json,svg,gif,jpg,jpeg,png,wav,ogg,webm,flac,aac,mp4,mp3',
       manifest: {
@@ -83,52 +49,7 @@ module.exports = function (ctx) {
         orientation: 'portrait',
         background_color: '#ffffff',
         theme_color: '#027be3',
-        icons: [
-          {
-            'src': 'statics/icons/icon-128x128.png',
-            'sizes': '128x128',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-192x192.png',
-            'sizes': '192x192',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-256x256.png',
-            'sizes': '256x256',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-384x384.png',
-            'sizes': '384x384',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-512x512.png',
-            'sizes': '512x512',
-            'type': 'image/png'
-          }
-        ]
       }
-    },
-    cordova: {
-      // id: 'org.cordova.quasar.app'
-    },
-    electron: {
-      extendWebpack (cfg) {
-        // do something with cfg
-      },
-      packager: {
-        // OS X / Mac App Store
-        // appBundleId: '',
-        // appCategoryType: '',
-        // osxSign: '',
-        // protocol: 'myapp://path',
-
-        // Window only
-        // win32metadata: { ... }
-      }
-    },
+    }
   }
 }
---
+++ src/pages/index.vue
@@ -0,0 +1,141 @@
+<template>
+  <q-page padding class="row NOflex NOflex-center">
+      <q-card
+        v-if="showCards.fullCalendar"
+        inline
+        class="full-width q-ma-sm"
+      >
+          <q-card-title>
+              Full calendar component
+              <span slot="subtitle">
+                    A multifunction component that displays calendar information in a variety of predefined formats.
+                </span>
+          </q-card-title>
+          <q-card-main>
+              <calendar
+                  :event-array="eventArray"
+                  :sunday-first-day-of-week="false"
+                  NOcalendar-locale="fr"
+                  NOcalendar-timezone="America/Los_Angeles"
+                  NOevent-ref="MYCALENDAR"
+                  :allow-editing="true"
+                  agenda-style="block"
+                  :render-html="true"
+                  :NOstart-date="new Date(2019, 1, 4)"
+              />
+          </q-card-main>
+      </q-card>
+      <q-card
+        v-if="showCards.month"
+        inline
+        class="full-width q-ma-sm"
+      >
+          <q-card-title>
+              Individual month view component
+              <span slot="subtitle">
+                    Example of a single component displayed. Acts independently of any other calendar component on the same page.
+                </span>
+          </q-card-title>
+          <q-card-main>
+              <calendar-month
+                  :event-array="eventArray"
+                  :sunday-first-day-of-week="false"
+                  calendar-locale="fr"
+              />
+          </q-card-main>
+      </q-card>
+      <q-card
+        v-if="showCards.week"
+        inline
+        class="full-width q-ma-sm"
+      >
+          <q-card-title>
+              Individual multi-day / week view component
+              <span slot="subtitle">
+                    The multi-day component. This can be configured as a proper full-week display (shown), a single day or a multi-day. The number of days shown and the number of days moved in the navigation are adjustable.
+                </span>
+          </q-card-title>
+          <q-card-main>
+              <calendar-multi-day
+                  :event-array="eventArray"
+                  scrollHeight="400px"
+              />
+          </q-card-main>
+      </q-card>
+      <q-card
+          v-if="showCards.agenda"
+          inline
+          class="full-width q-ma-sm"
+      >
+        <q-card-title>
+          Agenda view component
+        </q-card-title>
+        <q-card-main>
+          <calendar-agenda
+            :event-array="eventArray"
+            agenda-style="block"
+            :sunday-first-day-of-week="true"
+            :allow-editing="false"
+            :num-days="1"
+            calendar-locale="es"
+            calendar-timezone="America/Argentina/Buenos_Aires"
+          />
+        </q-card-main>
+      </q-card>
+  </q-page>
+</template>
+
+<script>
+import {
+  QPage,
+  QCard,
+  QCardTitle,
+  QCardMain,
+  QCardSeparator,
+  QIcon
+} from 'quasar'
+import {
+  Calendar,
+  CalendarMonth,
+  CalendarMultiDay,
+  CalendarAgenda
+} from '../components/calendar'
+import { sampleEventArray } from './page-mixins/sample-data'
+import MoveDates from './page-mixins/move-dates'
+
+export default {
+  name: 'PageIndex',
+  components: {
+    QPage,
+    QCard,
+    QCardTitle,
+    QCardMain,
+    QCardSeparator,
+    Calendar,
+    CalendarMonth,
+    CalendarMultiDay,
+    CalendarAgenda,
+    QIcon
+  },
+  mixins: [ MoveDates ],
+  data () {
+    return {
+      eventArray: sampleEventArray, // in page-mixins/sample-data.js
+      showCards: {
+        fullCalendar: true,
+        month: false,
+        week: false,
+        agenda: false
+      }
+    }
+  },
+  computed: {},
+  methods: {},
+  created () {
+    this.moveSampleDatesAhead()
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
